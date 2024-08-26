adbb1687ccb733ce273119847a002240e7ff131a
Chris Ware <c.p.ware@hotmail.com>
2024-08-25 17:37:21 -0700

    About Section copy update

--- components/AboutSection/AboutSection.tsx
+++ components/AboutSection/AboutSection.tsx
@@ -10,29 +10,6 @@ const AboutSection = () => {
         <div className={styles.content}>
           <div className={styles.left}>
             <div className={styles.miscContainer}>
-              <svg
-                width='0'
-                height='0'
-                xmlns='http://www.w3.org/2000/svg'
-                version='1.1'
-              >
-                <defs>
-                  <filter id='goo'>
-                    <feGaussianBlur
-                      in='SourceGraphic'
-                      stdDeviation='5'
-                      result='blur'
-                    />
-                    <feColorMatrix
-                      in='blur'
-                      mode='matrix'
-                      values='1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9'
-                      result='goo'
-                    />
-                    <feComposite in='SourceGraphic' in2='goo' operator='atop' />
-                  </filter>
-                </defs>
-              </svg>
               <div className={styles.imgContainer}>
                 <Image
                   src={Author}
@@ -45,36 +22,28 @@ const AboutSection = () => {
             </div>
           </div>
           <div className={styles.right}>
-            {/* <div className={styles.falseBtnContainer}>
-              <FalseButton btnType='secondary' text='About us' />
-            </div> */}
             <h2 className={`${styles.heading} h2v2`}>
               Who is
               <br />
               <span className={styles.span}> Chris Ware?</span>
             </h2>
             <p className={styles.text}>
-              Fonts & Footers is a digital agency focusing on web design, web
-              development, and SEO. We&apos;re experts at blending the latest
-              technology with creative ideas for top-notch results. By working
-              with us, you get personal care, support, and a website that boosts
-              engagement and sales. We opperate out of sunny Phoenix, AZ.
+              Because of my background in design, and finance, I understand the
+              aesthetic and business side to any project. While websites have to
+              make good impressions on their visitors, they should also motivate
+              them to action.
             </p>
             <br />
             <p className={styles.text}>
-              We use cutting-edge technology to craft websites that outperform
-              those built on platforms like Wordpress, Wix, or Godaddy, which
-              often rely on older, slower, and outdated technology. Our approach
-              involves using pure HTML, CSS, and Javascript code, ensuring a
-              sleek and efficient website.
+              I am skilled in both front-end and back-end development, and I
+              have the ability to create comprehensive web applications from
+              start to finish. My tools of choice are Next.js/React.
             </p>
             <br />
             <p className={styles.text}>
-              We use cutting-edge technology to craft websites that outperform
-              those built on platforms like Wordpress, Wix, or Godaddy, which
-              often rely on older, slower, and outdated technology. Our approach
-              involves using pure HTML, CSS, and Javascript code, ensuring a
-              sleek and efficient website.
+              I optimize web applications for speed, performance and SEO. They
+              all have fast load times and an optimal performance across various
+              devices and network conditions.
             </p>
             {/* <p className={styles.authorDetails}>
               ~ Chris Ware, Founder & CEO ~
